2d5acdfce4c922648627af6a2a6ffad452ba7985
armentamj <armentamj@gmail.com>
2026-08-03 18:43:11 +0200

    Modified the deploy.sh file to include deleteing static_prod without asking for permissions and forcfully.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 git pull
 
+# Stop the current containers to unlock file access
+docker compose down
+
+#Completely wipe the server's static folders
+rm -rf static_prod
+
 # Ensure folders exist
 mkdir -p static_prod
 chmod -R 777 static_prod 
@@ -14,7 +20,7 @@ sleep 5
 
 # Run migrations
 docker compose exec mdwp python manage.py migrate --noinput
-# Optional: Collect static files if you changed CSS/JS
-docker compose exec mdwp python manage.py collectstatic --noinput
+# Collect static files if you changed CSS/JS
+docker compose exec mdwp python manage.py collectstatic --noinput --clear
 
 echo "Deployment finished!"
