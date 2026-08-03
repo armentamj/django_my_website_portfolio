abc11859de7d0611eb0e1dddc210f109fee3d316
armentamj <armentamj@gmail.com>
2026-08-03 19:04:40 +0200

    Changed the deploy.sh file to stop the container and then restart  it.

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+echo "Pulling latest code from Git..."
 git pull
 
-# Stop the current containers to unlock file access
-docker compose down
-
 #Completely wipe the server's static folders
+echo "Cleaning old static files..."
 rm -rf static_prod
 
 # Ensure folders exist
+echo "Creating the static_prod directory..."
 mkdir -p static_prod
 chmod -R 777 static_prod 
 
+# Stopping the current containers to unlock file access
+echo "Stopping container..."
+docker compose down
+
 # Rebuild and start
-docker compose up -d --build
+echo "Building and starting container..."
+docker compose up -d --build --force-recreate
 
 # WAIT for the container to be ready
 echo "Waiting for container to start..."
@@ -20,7 +25,9 @@ sleep 5
 
 # Run migrations
 docker compose exec mdwp python manage.py migrate --noinput
+
 # Collect static files if you changed CSS/JS
+echo "Collecting static files..."
 docker compose exec mdwp python manage.py collectstatic --noinput --clear
 
 echo "Deployment finished!"
